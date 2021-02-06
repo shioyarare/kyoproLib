51b8f84ea0e3e39c32075fc7d9f3e47f008d4cc7
Shoya HARA <shioyarare@gmail.com>
2021-02-06 19:34:25 +0900

    feat: Creating the "Sieve of Eratosthenes" Algorithm.

---
+++ algorithm/sieve_of_eratosthenes.d
@@ -0,0 +1,23 @@
+/* Sieve of Eratosthenes
+ * O(n log log n)
+ */
+import std;
+const int max_n = 10_000_000;
+int[] prime; // nまでの素数
+auto is_prime = new bool[](max_n+1); // 篩
+
+void sieve(int n){
+	is_prime[2..$] = true;
+	foreach(i; 2..n+1){
+		if(is_prime[i]){
+			prime ~= i;
+			for(int j=2*i; j<=n; j+=i) is_prime[j] = false;
+		}
+	}
+}
+
+void main(){
+	int n = readln.chomp.to!int;
+	sieve(n);
+	prime.writeln();
+}
